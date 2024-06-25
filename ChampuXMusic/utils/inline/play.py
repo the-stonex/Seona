93f11c1253487a16ae0766d234b4f0684d2e2dae
The-stonex <147587252+the-stonex@users.noreply.github.com>
2024-06-25 06:23:07 +0530

    Update play.py

--- ChampuXMusic/utils/inline/play.py
+++ ChampuXMusic/utils/inline/play.py
@@ -68,8 +68,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-           InlineKeyboardButton(text=_["ᴏᴡɴᴇʀ"], url=f"https://t.me/iishiikathakur ),
-           InlineKeyboardButton(text="ᴄʜᴀᴛ", url=f"https://t.me/+4cEDA4oLWwQ5YzY1")
+           InlineKeyboardButton(text=_["ᴏᴡɴᴇʀ"], url=f"https://t.me/iishiikathakur"),
+           InlineKeyboardButton(text="ᴄʜᴀᴛ", url=f"https://t.me/+4cEDA4oLWwQ5YzY1") 
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
